b04839b0537ec79349e9613c8d8cc461386c55c4
LRN <2313598146@qq.com>
2022-04-07 18:38:36 +0800

    Initial commit

---
+++ src/main/java/com/kust/App.java
@@ -0,0 +1,13 @@
+package com.kust;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
